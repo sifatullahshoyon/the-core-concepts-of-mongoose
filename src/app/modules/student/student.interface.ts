5f5eff155a4a5e4bdf2ebc8114f4ea0debb0f8e8
freelancersifat627e <freelancersifat627@gmail.comm>
2024-11-23 19:22:24 +0600

    compleate create interface & schema

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,42 @@
+import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+
+export type Tguardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type TuserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TlocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Tstudent = {
+  id: string;
+  name: TuserName;
+  gender: 'male' | 'female'; // union type literal
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  BloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Tguardian;
+  localGuardian: TlocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'block';
+};
---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,95 @@
+import { Schema, model, connect } from 'mongoose';
+import {
+  Tguardian,
+  TlocalGuardian,
+  Tstudent,
+  TuserName,
+} from './student.interface';
+
+// 2. Create a Schema corresponding to the document interface.
+
+const userNameSchema = new Schema<TuserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Tguardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuardianSchema = new Schema<TlocalGuardian>({
+  name: {
+    type: String,
+    require: true,
+  },
+  occupation: {
+    type: String,
+    require: true,
+  },
+  contactNo: {
+    type: String,
+    require: true,
+  },
+  address: {
+    type: String,
+    require: true,
+  },
+});
+
+const studentSchema = new Schema<Tstudent>({
+  // interface এর ক্ষেত্রে type গুলো থাকে small letter আর mongoose এর ক্ষেত্রে type গুলো থাকে Capitalize
+  id: { type: String },
+  name: userNameSchema,
+  // typescript এ যেইটা union type mongoose এ সেইটা enum type
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: {
+    type: String,
+    required: true,
+  },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  BloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'block'],
+});
+
+// 3. Create a Model
+// variables এর নাম আর model এর নাম সেম রাখতে হয়।
+const Student = model<Tstudent>('Student', studentSchema);

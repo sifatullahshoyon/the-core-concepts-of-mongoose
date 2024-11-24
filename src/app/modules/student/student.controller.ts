8d818b34d4bc588ac83563e059881f30a26c89ed
freelancersifat627e <freelancersifat627@gmail.comm>
2024-11-24 15:59:35 +0600

    use joi custom validator packege

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from 'express';
 import { studentServices } from './student.service';
 
+import Joi from 'joi';
+
 const createStudent = async (req: Request, res: Response) => {
   try {
+    // creating a schema validation suing joi
+
+    const joivalidationSchema = Joi.object({
+      id: Joi.string(),
+      name: {
+        firstName: Joi.string().max(20).required(),
+        middleName: Joi.string().max(20),
+        lastName: Joi.string().max(20).required(),
+      },
+      gender: Joi.string().required().valid(['male', 'female', 'other']),
+    });
+
     const { student: studentData } = req.body;
     // will cal service func to send this data
     const result = await studentServices.createStudentIntoDB(studentData);

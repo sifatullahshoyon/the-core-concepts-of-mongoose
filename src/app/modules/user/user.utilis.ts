10499e609dfabae1e11ecbdb93c98890ff899695
freelancersifat627e <freelancersifat627@gmail.comm>
2024-12-10 01:26:27 +0600

    semester id bug fix

--- src/app/modules/user/user.utilis.ts
+++ src/app/modules/user/user.utilis.ts
@@ -17,12 +17,30 @@ const findLastStudentId = async () => {
       createdAt: -1,
     })
     .lean();
-
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  // exmple id: 2030 01 0001
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payLoad: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  // first time 0000
+  // 0001 => 1
+  let currentId = (0).toString(); // 0000 by default
+
+  const lastStudentId = await findLastStudentId();
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(0, 4); // 01
+  const lastStudentYear = lastStudentId?.substring(0, 4); // 2030
+  const currentSemesterCode = payLoad.code;
+  const currentYear = payLoad.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); // 0001
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payLoad.year}${payLoad.code}${incrementId}`;
